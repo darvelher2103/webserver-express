ffe6f0b8bff2334b6d4a22600314a21bba962f2f
Darwin <dvelasquezh21@gmail.com>
2018-08-30 11:46:29 -0500

    Agregamos el README

--- hbs/helpers.js
+++ hbs/helpers.js
@@ -1,13 +1,14 @@
 const hbs = require('hbs');
 
-//helpers
+//helpers que nos permiten realizar otras funciones en las vistas donde se carguen
 hbs.registerHelper('getAnio', () => {
     return new Date().getFullYear();
 });
+
 //helper que nos convierte texto en mayuscula
 hbs.registerHelper('capitalizar', (texto) => {
-    let palabras = texto.split(' ');
-    palabras.forEach((palabra, idx) => {
+    let palabras = texto.split(' '); //ELiminamos los espacios..
+    palabras.forEach((palabra, idx) => { //recorremos el array y convertimos el esto
         palabras[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
     });
 
--- server.js
+++ server.js
@@ -1,39 +1,32 @@
 const express = require('express');
 const app = express(); //utiliza internamente el module http
 const hbs = require('hbs');
+
 //helpers
 require('./hbs/helpers');
 
 //Esperando entrega del puerto por HEROKU
 const port = process.env.PORT || 3000
 
-//instruccion que se ejecuta sin importar la url middleword
-//carpeta publica (Posemos servir cualquier cantidad de archivos)
-app.use(express.static(__dirname + '/public'));
+//instruccion que se ejecuta sin importar la url middleware
+//carpeta publica (Poddemos servir cualquier cantidad de archivos.. index.html)
+app.use(express.static(__dirname + '/public')); //ruta donde se encuentran los archivos estaticos
 
 //Express HBS engine 
-hbs.registerPartials(__dirname + '/views/parciales'); //registramos los parciales
+hbs.registerPartials(__dirname + '/views/parciales'); //registramos los parciales para omitir las extensiones .html en las urls
 app.set('view engine', 'hbs');
 
-//servicio
-app.get('/', (req, res) => {
+//servicios a los que el usuario tiene acceso en nuestra pagina web
+app.get('/', (req, res) => { //Pagina de inicio
     res.render('home', {
         nombre: 'Darwin'
     });
-    //res.send('Hola Mundo')
-    /*let salida = {
-        nombre: 'Darwin',
-        edad: 30,
-        url: req.url
-    }*/
-    //formato JSON 
-    //res.send(salida);
 });
 
-app.get('/about', (req, res) => {
+app.get('/about', (req, res) => { //Pagina de about
     res.render('about');
 });
 
-app.listen(port, () => {
+app.listen(port, () => { //Metodo que me permite escuchar el puerto..
     console.log(`Escuchando peticiones en el puerto ${port}`);
-})
+});
